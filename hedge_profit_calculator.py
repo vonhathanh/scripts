447eeeb6a978c2758d4ba9d4e3ba075c122f3e95
hanhvn <vonhathanh@gmail.com>
2025-02-09 20:49:16 +0700

    refactor codes

--- hedge_profit_calculator.py
+++ hedge_profit_calculator.py
@@ -50,29 +50,31 @@ class Account:
             self.margin += trade.quantity * trade.price
 
     def close(self, price: float):
-        self.balance += (self.long.quantity - self.short.quantity) * price
-        self.balance += self.margin
+        self.balance += (self.long.quantity - self.short.quantity) * price + self.margin
         self.margin = 0.0
 
     def get_unrealized_pnl(self, price: float):
         return self.long.get_pnl(price) - self.short.get_pnl(price)
 
+    def get_pnl_according_to_prices(self, trades: list[Trade], prices: list[float]):
+        for trade in trades:
+            self.execute(trade)
+        for price in prices:
+            pnl = self.get_unrealized_pnl(price)
+            print(f"pnl after close the trades at {price} is: ", pnl)
 
-trades = [
-    Trade("buy", 650.0, 1.0),
-    Trade("sell", 640.0, 0.25),
-    Trade("sell", 620.0, 0.25)
-]
-
-acc = Account(10000.0)
-
-
-def get_pnl_according_to_prices(trades: list[Trade], prices: list[float]):
-    for trade in trades:
-        acc.execute(trade)
-    for price in prices:
-        pnl = acc.get_unrealized_pnl(price)
-        print(f"pnl after close the trades at {price} is: ", pnl)
 
 if __name__ == '__main__':
-    get_pnl_according_to_prices(trades, [600.0, 500.0, 400.0, 660.0, 700.0])
+
+    trades = [
+        Trade("buy", 650.0, 1.0),
+        Trade("sell", 640.0, 0.25),
+        Trade("sell", 620.0, 0.25)
+    ]
+
+    acc = Account(10000.0)
+
+    acc.get_pnl_according_to_prices(trades, [600.0, 500.0, 400.0, 660.0, 700.0])
+    acc.close(700)
+
+    print(acc.balance)
