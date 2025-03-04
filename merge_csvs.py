4846c74e75c63c8d393983f99eb71273bc26ca58
hanhvn <vonhathanh@gmail.com>
2025-03-04 08:35:46 +0700

    fix bug merged csv miss start date due to header is lost

--- merge_csvs.py
+++ merge_csvs.py
@@ -5,20 +5,26 @@ import re
 
 import pandas as pd
 
-useless_columns = ["Volume",
-                   "Quote asset volume",
-                   "Number of trades",
-                   "Taker buy base asset volume",
-                   "Taker buy quote asset volume",
-                   "Ignore"]
+
 final_columns = ["Open time", "Open", "High", "Low", "Close", "Close time"]
+columns = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume",
+               "Number of trades", "Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"]
+
+
+def is_header(strings: list[str]) -> bool:
+    # header is a list of strings that can't be parsed to integer
+    try:
+        int(strings[0])
+    except ValueError:
+        return True
+    return False
+
 
 def merge(path: str, output_file: str = "final.csv", start_month="", end_month=""):
 
     files = glob.glob(os.path.join(path, "*.csv"))
     files = sorted(files)
-    columns = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume",
-               "Number of trades", "Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"]
+
     dfs = []
     # merge method: read all csv files, save the dataframes to an array and then use pd.concat to merge them
     # the loop below is the reading data part
@@ -34,20 +40,22 @@ def merge(path: str, output_file: str = "final.csv", start_month="", end_month="
             continue
 
         print("processing file: ", file)
-        df = pd.read_csv(file)
 
-        if df.columns.tolist() != final_columns:
-            # file has no column header, so we must add one before remove them
-            df.columns = columns
-            # save file with redudant columns removed
-            df.to_csv(file, index=False,
-                      columns=final_columns)
+        with open(file, "r") as f:
+            first_line = f.readline().strip().split(",")
 
+        if is_header(first_line):
+            df = pd.read_csv(file)
+        else:
+            df = pd.read_csv(file, names=columns)
+        # assign columns header for files that doesn't have the header
+        df.columns = columns
         dfs.append(df)
 
     # actually merge data happens here
     df = pd.concat(dfs, ignore_index=True)
-    # df may contains redudant columns so columns=final_columns is required
+
+    # df may contains redundant columns so columns=final_columns is required
     df.to_csv(os.path.join(path, output_file), index=False, columns=final_columns)
 
 
