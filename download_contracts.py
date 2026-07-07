925ad3faaf237c89684d853668d9586d80422311
hanhvn <vonhathanh@gmail.com>
2026-07-07 22:00:07 +0700

    update download contract script to work with etherscan api v2

--- download_contracts.py
+++ download_contracts.py
@@ -7,19 +7,19 @@ import os.path
 
 import requests
 
-URL = {
-    "eth": "api.etherscan.io",
-    "polygon": "api.polygonscan.io"
+CHAIN_ID     = {
+    "eth": 1,
+    "polygon": 137
 }
 
-DOWNLOAD_URL = "https://{url}/api?module=contract&action=getsourcecode&address={address}&apikey={apikey}"
+DOWNLOAD_URL = "https://api.etherscan.io/v2/api?module=contract&action=getsourcecode&address={address}&apikey={apikey}&chainid={chain_id}"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("address",)
     parser.add_argument("network")
-    parser.add_argument("apikey")
+    parser.add_argument("--apikey", help="Blockchain explorer API key, default = None", default=None)
     parser.add_argument("--out_dir", help="output path (relative with current dir), default = ./out",
                         default="./out")
 
@@ -27,19 +27,19 @@ if __name__ == '__main__':
 
     address = args["address"]
     network = args["network"]
-    apikey = args["apikey"]
+    apikey = args["apikey"] or os.environ.get("API_KEY")
     out_dir = args["out_dir"]
 
-    url = DOWNLOAD_URL.format(url=URL[network], address=address, apikey=apikey)
+    url = DOWNLOAD_URL.format(chain_id=CHAIN_ID[network], address=address, apikey=apikey)
 
     response = requests.get(url)
 
     if response.status_code != 200:
         raise Exception(f"download contract failed, reason: {response.reason}")
 
-    data = json.loads(response.text)["result"][0]
+    data = json.loads(response.text)["result"]
 
-    sources = json.loads(data["SourceCode"][1:-1])["sources"]
+    sources = json.loads(data[0]["SourceCode"][1:-1])["sources"]
 
     os.makedirs(out_dir, exist_ok=True)
 
